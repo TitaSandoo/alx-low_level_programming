530b3e4cc75d48c1872ff75dd865f86e3bfb0bf0
TitaSandooalx-system_engineering-devops <fa.moh.ahmed@gmail.com>
2023-09-13 00:54:05 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,17 @@
  * main - print _putchar
  * Return: Always 0
  */
+
 int main(void)
 {
 putchar('');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
